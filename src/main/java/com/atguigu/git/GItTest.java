3a5a0545f047eb7d3d9234ae41d90a0e98703ce4
jsz <986403754@qq.com>
2023-02-19 01:38:12 +0800

    4 hot

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -8,6 +8,7 @@ public class GItTest {
         System.out.println("hello git!");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4 hot");
 
     }
 }
