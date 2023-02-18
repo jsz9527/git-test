4a25513fb2745320ab211723ca650da549f3a3fb
jsz <986403754@qq.com>
2023-02-19 01:26:15 +0800

    2

---
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author layne
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("2");
+
+    }
+}
